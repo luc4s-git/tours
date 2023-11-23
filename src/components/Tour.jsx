662b527fc17ed4eb3f0e91b3304243a2c0610bc4
luc4s-git <lucassantos4332@hotmail.com>
2023-11-23 15:37:38 -0300

    read more function jeito certo

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,38 +1,7 @@
 import { useState } from 'react';
 
 export default function Tour({ id, name, info, image, price, deleteTour }) {
-  const [readMore, setReadMore] = useState(true);
-  const [isFull, setIsFull] = useState(false);
-  const [textInfo, setTextInfo] = useState(info);
-
-  const ShowReadMore = () => {
-    return (
-      <>
-        <button
-          type="button"
-          className="info-btn"
-          onClick={() => {
-            setReadMore(!readMore);
-            setIsFull(!isFull);
-          }}
-        >
-          read more
-        </button>
-      </>
-    );
-  };
-
-  const ShowText = () => {
-    if (!isFull) {
-      setTextInfo(info.slice(0, 200));
-    }
-
-    if (isFull) {
-      setTextInfo(info);
-    }
-
-    return <p>{textInfo}</p>;
-  };
+  const [readMore, setReadMore] = useState(false);
 
   return (
     <article className="single-tour">
@@ -40,8 +9,17 @@ export default function Tour({ id, name, info, image, price, deleteTour }) {
       <span className="tour-price">${price}</span>
       <div className="tour-info">
         <h5>{name}</h5>
-        <ShowText />
-        <ShowReadMore />
+        <p>
+          {readMore ? info : `${info.slice(0, 200)}...`}{' '}
+          <button
+            className="info-btn"
+            onClick={() => {
+              setReadMore(!readMore);
+            }}
+          >
+            {readMore ? ' show less' : 'show more'}
+          </button>
+        </p>
         <button
           type="buton"
           className="btn btn-block delete-btn"
