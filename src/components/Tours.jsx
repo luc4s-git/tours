181c3f36880a3605ad5ed37bb2ef15524e1d93ad
luc4s-git <lucassantos4332@hotmail.com>
2023-11-23 17:12:41 -0300

    meu try reFetch data

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -7,18 +7,28 @@ export default function Tours({ tours, deleteTour, reFetchData }) {
     return <Tour key={tour.id} {...tour} deleteTour={deleteTour}></Tour>;
   });
 
-  return (
-    <section>
-      <div className="title">
-        <h2>our tours</h2>
-        <div className="title-underline"></div>
-        <div className="tours">{toursMapping}</div>
+  const CheckLength = () => {
+    if (tours.length === 0) {
+      return (
         <IoMdRefresh
           style={{ cursor: 'pointer', height: '10rem', width: '10rem' }}
           onClick={() => {
             reFetchData();
           }}
         />
+      );
+    } else {
+      return <h1></h1>;
+    }
+  };
+
+  return (
+    <section>
+      <div className="title">
+        <h2>our tours</h2>
+        <div className="title-underline"></div>
+        <div className="tours">{toursMapping}</div>
+        <CheckLength />
       </div>
     </section>
   );
