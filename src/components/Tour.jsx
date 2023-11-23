cf64c58d2b71778919872ebd7d8deb79dc1e7123
luc4s-git <lucassantos4332@hotmail.com>
2023-11-23 14:58:27 -0300

    read more

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,15 +1,32 @@
+import { useState } from 'react';
+
 export default function Tour({ id, name, info, image, price, deleteTour }) {
+  const [isReadMore, setIsReadMore] = useState(true);
+  const [tourInfo, setTourInfo] = useState(isReadMore && info.slice(0, 200));
+
   return (
     <article className="single-tour">
       <img src={image} alt={name + 'tour'} className="img" />
       <span className="tour-price">${price}</span>
       <div className="tour-info">
         <h5>{name}</h5>
-        <p>{info}</p>
+        <p>{tourInfo}</p>
+        <button
+          type="button"
+          className="info-btn"
+          onClick={() => {
+            setTourInfo(info);
+            setIsReadMore(false);
+          }}
+        >
+          read more
+        </button>
         <button
           type="buton"
           className="btn btn-block delete-btn"
-          onClick={() => deleteTour(id)}
+          onClick={() => {
+            deleteTour(id);
+          }}
         >
           not interested
         </button>
