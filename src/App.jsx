07afee2dd53bef8bf6bd62a85e0951539325f234
luc4s-git <lucassantos4332@hotmail.com>
2023-11-23 19:04:04 -0300

    refetch button acshually

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import Loading from './components/Loading';
 import Tours from './components/Tours';
+import refresh from './assets/refresh.svg';
 
 const url = 'https://course-api.com/react-tours-project';
 
@@ -35,6 +36,27 @@ const App = () => {
     fetchData();
   }, []);
 
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <div className="title-underline"></div>
+          <img
+            src={refresh}
+            alt="refresh button"
+            style={{
+              cursor: 'pointer',
+              margin: '2rem',
+              height: '5rem',
+              width: '5rem',
+            }}
+            onClick={() => fetchData()}
+          />
+        </div>
+      </main>
+    );
+  }
   return (
     <main>
       {isLoading ? (
--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,34 +1,19 @@
 import { useState } from 'react';
 import Tour from './Tour';
-import { IoMdRefresh } from 'react-icons/io';
 
 export default function Tours({ tours, deleteTour, reFetchData }) {
+  const [title, setTitle] = useState('our tours');
+
   const toursMapping = tours.map((tour) => {
     return <Tour key={tour.id} {...tour} deleteTour={deleteTour}></Tour>;
   });
 
-  const CheckLength = () => {
-    if (tours.length === 0) {
-      return (
-        <IoMdRefresh
-          style={{ cursor: 'pointer', height: '10rem', width: '10rem' }}
-          onClick={() => {
-            reFetchData();
-          }}
-        />
-      );
-    } else {
-      return <h1></h1>;
-    }
-  };
-
   return (
     <section>
       <div className="title">
-        <h2>our tours</h2>
+        <h2>{title}</h2>
         <div className="title-underline"></div>
         <div className="tours">{toursMapping}</div>
-        <CheckLength />
       </div>
     </section>
   );
